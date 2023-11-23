09b558703bdcd69aa563301f7d21772f9f7ec2e8
Oruj Mehrabov <orujvm@code.edu.az>
2023-11-23 13:34:43 +0400

    Add project files.

---
+++ Domain/Models/SignUp.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class SignUp : BaseEntity
+    {
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
